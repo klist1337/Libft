c7312c5d0ea762da2576e6c5bb81efec3e1e1850
klist1337 <58371768+klist1337@users.noreply.github.com>
2021-11-17 14:16:54 +0100

    Add files via upload

---
+++ Libft/ft_lstnew.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_listnew.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eassofi <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/16 09:27:42 by eassofi           #+#    #+#             */
+/*   Updated: 2021/11/16 13:20:15 by eassofi          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*node;
+
+	node = (t_list *)malloc(sizeof(t_list));
+	if (node == 0)
+		return (0);
+	node ->content = content;
+	node ->next = NULL;
+	return (node);
+}
